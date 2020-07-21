1b81b5217c30b02f289043e8c421742c6caf5307
Abdelmomen <34581449+Abdelmmomen@users.noreply.github.com>
2020-07-21 21:09:21 +0200

    Responsive

---
+++ js/plugin.js
@@ -0,0 +1,33 @@
+$(function() {
+    "use strict"
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() > "200px") {
+            console.log("yes")
+        }
+    })
+
+    // if ($(window).scroll() > "200px") {
+    //     console.log("good")
+    // } else {
+    //     console.log("bad")
+    // }
+
+    // if ($("header").offset().top > "200px") {
+    //     console.log("good")
+    // } else {
+    //     console.log("bad")
+    // }
+
+})
+
+// function myFunction() {
+
+// if (window.scrollHeight >= "100px") {
+//     console.log("yes")
+// }
+// }
+
+// if (window.scrollHeight >= "100px") {
+//     console.log("yes")
+// }
